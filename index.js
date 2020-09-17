64065e1605bc302350b27a22d940663cfebe726f
Erica <67807566+ericasu33@users.noreply.github.com>
2020-09-17 15:35:31 -0700

    Add files via upload
    removes some unnecessary codes

---
+++ index.js
@@ -0,0 +1,63 @@
+
+const getUserChoice = () => { //userInput here = undefined
+  userInput = prompt("Rock, Paper, or Scissors: ").toLowerCase(); //userInput is assigned value here.
+  if (userInput === "rock" || userInput === "paper" || userInput === "scissors") {
+    return userInput;
+  } else {
+    console.log("Please input one of the following: rock, paper, or scissors. ")
+    return getUserChoice(); //calls the function again (try again) 
+  }
+};
+
+const getComputerChoice = () => {
+  let num = Math.floor(Math.random() * 3);
+  switch (num) {
+    case 0:
+      return "rock";
+      break;
+    case 1:
+      return "paper";
+      break;
+    default:
+      return "scissors";
+      break;
+  }
+}
+
+const determineWinner = (userChoice, computerChoice) => {
+  if (userChoice === computerChoice) {
+    return "Tie";
+  }
+
+  if (userChoice === "rock"){
+    if (computerChoice === "paper"){
+      return "computer won!";
+    } 
+    return "you won!";
+  }
+
+  if (userChoice === "paper"){
+    if (computerChoice === "scissors"){
+      return "computer won!";
+    }
+    return "you won!";
+  }
+
+  if (userChoice === "scissors"){
+    if (computerChoice === "rock"){
+      return "computer won!";
+    }
+    return "you won!";
+  }
+}
+
+const playGame = ()=> {
+  const userChoice = getUserChoice();
+  const computerChoice = getComputerChoice();
+
+  console.log("User Choice: " + userChoice);
+  console.log("Computer Choice: " + computerChoice);
+  console.log(determineWinner(userChoice, computerChoice));
+}
+
+playGame();
